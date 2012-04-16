85060a4c0a49708f530ed73bb4c286b08c35f054
leocrawford <leocrawford@d0a68483-6ec2-4323-88a6-4402e20ead53>
2012-04-16 17:43:48 +0000

    Remove all code prior to checkin of v2

--- trunk/src/monkeypuzzle/io/parser/plist/PListArray.java
+++
@@ -1,8 +0,0 @@
-package monkeypuzzle.io.parser.plist;
-
-import java.util.List;
-
-public interface PListArray extends PListContainer, List<PListContainer>
-{
-
-}
--- trunk/src/monkeypuzzle/io/util/Util.java
+++
@@ -1,7 +0,0 @@
-package monkeypuzzle.io.util;
-
-public class Util
-{
-	public static final String SYSTEM_LINE_SEPARATOR = System
-			.getProperty("line.separator");
-}
