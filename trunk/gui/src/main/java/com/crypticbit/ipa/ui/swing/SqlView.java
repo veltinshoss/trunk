2be29b212dc55ea1600c97dcf25216b0d6f1ef22
leocrawford <leocrawford@d0a68483-6ec2-4323-88a6-4402e20ead53>
2012-12-22 18:11:12 +0000

    moved away from sqljet for database browser. Used modification from http://www.java2s.com/Code/Java/Database-SQL-JDBC/DatabaseBrowser.htm

--- trunk/gui/src/main/java/com/crypticbit/ipa/ui/swing/SqlView.java
+++ trunk/gui/src/main/java/com/crypticbit/ipa/ui/swing/SqlView.java
@@ -5,10 +5,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import javax.swing.JFrame;
-
-import org.tmatesoft.sqljet.browser.core.BrowserComponentManager;
-
 import com.crypticbit.ipa.central.FileParseException;
 import com.crypticbit.ipa.central.backupfile.BackupFile;
 import com.crypticbit.ipa.io.parser.sqlite.SqlResults;
@@ -19,65 +15,65 @@ import com.crypticbit.ipa.ui.swing.Mediator.HighlightChangeListener;
 @SuppressWarnings("serial")
 public class SqlView extends View implements HighlightChangeListener {
 
-	private Collection<Location> highlights = new ArrayList<Location>();
-	private BackupFile bfd;
+    private Collection<Location> highlights = new ArrayList<Location>();
+    private BackupFile bfd;
 
-	SqlView(final BackupFile bfd, final Mediator mediator) throws IOException,
-			FileParseException {
-		super(bfd, mediator);
-		this.bfd = bfd;
-		setLayout(new GridLayout(1, 1));
-	}
+    SqlView(final BackupFile bfd, final Mediator mediator) throws IOException,
+	    FileParseException {
+	super(bfd, mediator);
+	this.bfd = bfd;
+	setLayout(new GridLayout(1, 1));
+    }
 
-	@Override
-	public void clearHighlighting() {
-		this.highlights = new ArrayList<Location>();
-	}
+    @Override
+    public void clearHighlighting() {
+	this.highlights = new ArrayList<Location>();
+    }
 
-	@Override
-	public void highlight(final Collection<Location> locations) {
-		// FIXME
-		// for (Location l : locations)
-		// {
-		// if ((l.getContentType() == ContentType.SQL)
-		// && (l.getBackupFile() == getBackupFile()))
-		// {
-		// this.highlights.add(l);
-		// }
-		// }
-	}
+    @Override
+    public void highlight(final Collection<Location> locations) {
+	// FIXME
+	// for (Location l : locations)
+	// {
+	// if ((l.getContentType() == ContentType.SQL)
+	// && (l.getBackupFile() == getBackupFile()))
+	// {
+	// this.highlights.add(l);
+	// }
+	// }
+    }
 
-	@Override
-	public void moveTo0(final Location location) {
-		// int tabIndex = this.tabbedPane.indexOfTab((((SqlLocation) location)
-		// .getTable()));
-		// if (tabIndex != -1)
-		// {
-		// this.tabbedPane.setSelectedIndex(tabIndex);
-		// }
-	}
+    @Override
+    public void moveTo0(final Location location) {
+	// int tabIndex = this.tabbedPane.indexOfTab((((SqlLocation) location)
+	// .getTable()));
+	// if (tabIndex != -1)
+	// {
+	// this.tabbedPane.setSelectedIndex(tabIndex);
+	// }
+    }
 
-	@Override
-	protected ContentType getSupportedContentView() {
-		return ContentType.SQL;
-	}
+    @Override
+    protected ContentType getSupportedContentView() {
+	return ContentType.SQL;
+    }
 
-	@Override
-	protected void init() {
-		BrowserComponentManager manager = BrowserComponentManager
-				.create(new JFrame());
-		this.add(manager.getComponent());
-		try {
-			manager.open(bfd.getContentsFile());
-		} catch (IOException e) {
-			getMediator().displayErrorDialog(
-					"Unable to access database file: "
-							+ bfd.getOriginalFileName(), e);
-		}
+    @Override
+    protected void init() {
+	try {
+	    DatabaseBrowser databaseBrowser = new DatabaseBrowser();
+	    this.add(databaseBrowser);
+	    databaseBrowser.open(bfd);
+	} catch (Exception e) {
+	    getMediator().displayErrorDialog(
+		    "Unable to access database file: "
+			    + bfd.getOriginalFileName(), e);
 	}
+    }
 
-	@Override
-	protected boolean shouldBeVisible() {
-		return getBackupFile().getParsedData() instanceof SqlResults;
-	}
+    @Override
+    protected boolean shouldBeVisible() {
+	return getBackupFile().getParsedData() instanceof SqlResults;
+    }
+
 }
